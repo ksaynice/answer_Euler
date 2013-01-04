8e6ac1b48eb26d549397294e1da536ef6c04a6cd
K <ksaynice@gmail.com>
2013-01-04 13:18:14 -0800

    modify 3, should add two array not push

--- 003_largest_prime_factor.rb
+++ 003_largest_prime_factor.rb
@@ -1,9 +1,9 @@
 def prime_factors(n)
   return [] if n == 1
   factors = (2..n).find{|v| n % v == 0}
-  [factors] << prime_factors(n / factors)
+  [factors] + prime_factors(n / factors)
 end
 
 largest_prime_factor = prime_factors(600851475143)
 
-puts largest_prime_factor
+puts largest_prime_factor.max
