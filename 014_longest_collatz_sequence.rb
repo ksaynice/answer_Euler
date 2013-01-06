55ca10287f160ef6797bffcb64015e099633d667
K <ksaynice@gmail.com>
2013-01-06 13:53:05 -0800

    modified 14

--- 014_longest_collatz_sequence.rb
+++ 014_longest_collatz_sequence.rb
@@ -1,3 +1,33 @@
+def collatz(n)
+  i = 1
+  until n == 1
+    if n.even?
+      n /= 2
+    elsif n.odd?
+      n = 3 * n + 1
+    end
+    i += 1
+  end
+  i
+end
+
+from = 1
+to = 1000000
+biggest_count = 0
+strating_num = 0
+
+(from..to).each do |x|
+  if biggest_count < collatz(x)
+    biggest_count = collatz(x)
+    strating_num = x
+  end
+end
+puts biggest_count
+puts strating_num
+
+__END__
+
+---------- if check sequence numbs (slow 65s+)----------
 def collatz(n, i)
   if n != 1
     if n.even?
