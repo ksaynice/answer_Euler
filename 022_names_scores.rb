a87a90ed4360f7ce136c5ac445f88a7a6ef9fe64
K <ksaynice@gmail.com>
2013-01-12 21:31:23 -0800

    solution to 22

--- 022_names_scores.rb
+++ 022_names_scores.rb
@@ -1,3 +1,9 @@
 # sum of alphabetical index of string
-#'ABCD'.bytes.inject(0) { |sum, a| sum + a - 64 }
+#'ABCD'.bytes.inject(0) { |sum, n| sum + n - 64 }
 
+names = File.read("022_names.txt").gsub!(/\"/,"").split(",").map(&:strip).sort!
+scores = 0
+names.each.with_index do |x, i|
+  scores += x.bytes.inject(0) { |sum, n| sum + n - 64} * (i + 1)
+end
+puts scores
