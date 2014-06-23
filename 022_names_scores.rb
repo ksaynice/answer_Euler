66895a87a64b1510ddeb4c1bc6ff5dc0cff8abc3
K <ksaynice@gmail.com>
2014-06-23 14:04:04 -0700

    22 tweak for folder move later

--- 022_names_scores.rb
+++ 022_names_scores.rb
@@ -1,7 +1,7 @@
 # sum of alphabetical index of string
 #'ABCD'.bytes.inject(0) { |sum, n| sum + n - 64 }
 
-names = File.read("022_names.txt").gsub!(/\"/,"").split(",").map(&:strip).sort!
+names = File.read("./022_names.txt").gsub!(/\"/,"").split(",").map(&:strip).sort!
 scores = 0
 names.each.with_index do |x, i|
   scores += x.bytes.inject(0) { |sum, n| sum + n - 64} * (i + 1)
